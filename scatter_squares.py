450dea7c6f5a778ac4c6519757b74d412e2c08e6
Clayton <cdston@gmail.com>
2022-07-12 19:37:17 -0300

    calculando automaticamente

--- scatter_squares.py
+++ scatter_squares.py
@@ -2,10 +2,10 @@ from shutil import which
 import matplotlib.pyplot as plt
 
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=100)
+plt.scatter(x_values, y_values, s=40)
 #Define o titulo do grafico e nomeira os eixos 
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
@@ -13,4 +13,7 @@ plt.ylabel("Square of Value", fontsize=14)
 
 #Define o tamanho dos rótulos das marcações 
 plt.tick_params(axis='both', which='major', labelsize=14)
+
+#Define o intervalo para cada eixo
+plt.axis([0, 1100, 0, 1100000])
 plt.show()
