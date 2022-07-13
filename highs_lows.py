d0085ce0df0730d2971287e6f417aabd60323522
Clayton <cdston@gmail.com>
2022-07-13 17:49:14 -0300

    verificando erros

--- highs_lows.py
+++ highs_lows.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 #Obtem as temperaturas maximas do arquivo
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
@@ -14,12 +14,16 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-        high = int(row[1]) 
-        highs.append(high)
-        low = int(row[3])
-        lows.append(low)
+        try: 
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1]) 
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:    
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
        
     
 
@@ -30,7 +34,8 @@ plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 #Formata o grafico
-plt.title("Daily high and lows temperatures - 2014", fontsize=24)
+title = "Daily high and lows temperatures - 2014\nDeath Valley, CA"
+plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
