d5d4aca6cc17d5859096a86336d0bf946c6963e0
Clayton <cdston@gmail.com>
2022-07-13 09:38:28 -0300

    gerando passeios aleatorios

--- rw_visual.py
+++ rw_visual.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-#Cria um passeio aleatorio e plota os pontos
+# Continua criando novos passeis enqunto o program estiver ativo
+while True:
+    #Cria um passeio aleatorio e plota os pontos
+    rw = RandomWalk()
+    rw.fill_walk()
 
-rw = RandomWalk()
-rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
 
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    keep_runnning = input("Make another? (y/n): ")
+    if keep_runnning == 'n': break
