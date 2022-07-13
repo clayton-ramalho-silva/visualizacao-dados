4fd1e6f705c8b44b343cc1fcdc38c0b4b1fc0f3f
Clayton <cdston@gmail.com>
2022-07-13 09:54:52 -0300

    alterando tamanho da tela

--- rw_visual.py
+++ rw_visual.py
@@ -4,10 +4,23 @@ from random_walk import RandomWalk
 # Continua criando novos passeis enqunto o program estiver ativo
 while True:
     #Cria um passeio aleatorio e plota os pontos
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+    #Define tamanho da janela de plotagem
+    plt.figure(figsize=(10, 6))
+
+    point_numbers = list(range(rw.num_points))
+
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
+    #Enfatiza o primeiro e o ultimo ponto
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    
+    #Remove eixos
+    #plt.axes().get_xaxis().set_visible(False)
+    #plt.axes().get_yaxis().set_visible(False)
+    
     plt.show()
 
     keep_runnning = input("Make another? (y/n): ")
